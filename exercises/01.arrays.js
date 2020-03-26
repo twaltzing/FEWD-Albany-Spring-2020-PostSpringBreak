e73c87627a2756e7a436546dd816da4a6e44eb4d
twaltzing <terry.waltz@chineseandspanish.com>
2020-03-26 19:23:40 -0400

    array exercises 20200326 class

--- exercises/01.arrays.js
+++ exercises/01.arrays.js
@@ -10,26 +10,25 @@
  * @description
  * create an arrays of names
  */
-
+names = ['Cindy', 'Jan', 'Marsha', 'Bobby', 'Peter', 'Greg', 'Alice', 'Sam the Butcher'];
+for (i=0; i<names.length; i++){
+console.log(names[i]);
+}
 /**
  * @description
  * access the first item in the array above
  */
-
+first = names[0];
+console.log("The first name in the array is " +first);
 /**
  * @description
  * access the last character in the first item in the array above
  */
-
+lastCharPos = first.length-1;
+// console.log("Position is "+ lastCharPos);
+lastChar = first[lastCharPos];
+console.log("The last character of the first name ("+first+") is " +lastChar);
 /**
  * @do
  * include this file in a new pull request
  */
-
-
- git add . 
- git commit -m ' ... '
- git pull upstream master
-
-
- 
